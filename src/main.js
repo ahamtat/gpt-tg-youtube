b63e86dc68733d7b714f2338921cd98f1d7b1500
Artem Orlov <ahamtat108@gmail.com>
2023-06-22 09:53:11 +0300

    added OpenAI API call with converted audio file

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import { code } from "telegraf/format";
 import config from "config";
 import { ogg } from "./ogg.js"
+import { openai } from "./openai.js";
 
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
 
 // processing voice messages
 bot.on(message("voice"), async (ctx) => {
     try {
+        await ctx.reply(code("processing your voice message..."));
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
         const userId = String(ctx.message.from.id);
         const oggPath = await ogg.create(link.href, userId);
         const mp3Path = await ogg.toMP3(oggPath, userId);
-        await ctx.reply(mp3Path);
+
+        const text = await openai.transcription(mp3Path);
+        await ctx.reply(code(`Your request: "${text}"`));
+
+        const messages = [{ role: openai.roles.USER, content: text }];
+        const response = await openai.chat(messages);
+
+        await ctx.reply(response.content);
 
     } catch (error) {
         console.error("error while voice message: ", error.message);
